f209eb3728769d36829c831923db2686945dba46
Evgeniya Shevyakova <stativa.zhe@yandex.ru>
2023-09-08 13:52:23 +0300

    add image to Documents

---
+++ DocumentsApp/DocumentsController.swift
@@ -0,0 +1,75 @@
+//
+//  DocumentsController.swift
+//  DocumentsApp
+//
+//  Created by Евгения Шевякова on 06.09.2023.
+//
+
+import UIKit
+
+class DocumentsController: UITableViewController {
+    
+    var model: Model?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        model = Model(path: NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0])
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return model?.items.count ?? 0
+    }
+
+    @IBAction func addPhotoAction(_ sender: Any) {
+        showImagePickerController()
+        tableView.reloadData()
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        var configuration = UIListContentConfiguration.cell()
+        configuration.text = model?.items[indexPath.row]
+        
+        cell.contentConfiguration = configuration
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            model?.deleteItem(withIndex: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+}
+
+extension DocumentsController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    func showImagePickerController() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            model?.addImage(image: image)
+        }
+
+        dismiss(animated: true)
+        tableView.reloadData()
+    }
+}
---
+++ DocumentsApp/Model.swift
@@ -0,0 +1,41 @@
+//
+//  Model.swift
+//  DocumentsApp
+//
+//  Created by Евгения Шевякова on 06.09.2023.
+//
+
+import UIKit
+
+class Model {
+    
+    var path: String
+
+    init(path: String) {
+        self.path = path
+    }
+
+    var items: [String] {
+        return (try? FileManager.default.contentsOfDirectory(atPath: path)) ?? []
+    }
+
+    func addImage(image: UIImage) {
+
+        let name = UUID().uuidString
+        let url = URL(filePath: path).appending(component: name)
+
+        if let data = image.pngData() {
+            do {
+                try data.write(to: url)
+            } catch {
+                print("Unable to write image data to disk")
+                print(error.localizedDescription)
+            }
+        }
+    }
+
+    func deleteItem(withIndex index: Int) {
+        let pathForDelete = path + "/" + items[index]
+        try? FileManager.default.removeItem(atPath: pathForDelete)
+    }
+}
